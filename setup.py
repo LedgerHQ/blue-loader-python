70233d1d9ab823d74a4a51791bcb483ce506a210
BTChip <hello@ledger.fr>
2016-05-20 08:36:59 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os.path import dirname, join
 here = dirname(__file__)
 setup(
     name='ledgerblue',
-    version='0.1.1',
+    version='0.1.2',
     author='Ledger',
     author_email='hello@ledger.fr',
     description='Python library to communicate with Ledger Blue',
