71c0b9ffa5881a74eb08d5fecc131670eab5e04b
BTChip <hello@ledger.fr>
2016-07-28 11:47:04 +0200

    Add locked down application path, icon, signature support

--- ledgerblue/loadApp.py
+++ ledgerblue/loadApp.py
@@ -18,19 +18,36 @@
 """
 
 from secp256k1 import PrivateKey
-from ledgerblue.comm import getDongle
-from ledgerblue.hexParser import IntelHexParser
-from ledgerblue.hexLoader import HexLoader
-from ledgerblue.deployed import getDeployedSecretV1, getDeployedSecretV2
+from .comm import getDongle
+from .hexParser import IntelHexParser
+from .hexLoader import HexLoader
+from .deployed import getDeployedSecretV1, getDeployedSecretV2
 import argparse
+import struct
 
 def auto_int(x):
     return int(x, 0)
 
+def parse_bip32_path(path):
+        if len(path) == 0:
+                return ""
+        result = ""
+        elements = path.split('/')
+        for pathElement in elements:
+                element = pathElement.split('\'')
+                if len(element) == 1:
+                        result = result + struct.pack(">I", int(element[0]))             
+                else:
+                        result = result + struct.pack(">I", 0x80000000 | int(element[0]))
+        return result
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--targetId", help="Set the chip target ID", type=auto_int)
 parser.add_argument("--fileName", help="Set the file name to load")
+parser.add_argument("--icon", help="Set the icon content to use (hex encoded)")
+parser.add_argument("--path", help="BIP 32 path to which the derivation is locked (format decimal a'/b'/c)")
 parser.add_argument("--appName", help="Set the application name")
+parser.add_argument("--signature", help="Optional application's signature (hex encoded)")
 parser.add_argument("--appFlags", help="Set the application flags", type=auto_int)
 parser.add_argument("--bootAddr", help="Set the boot address", type=auto_int)
 parser.add_argument("--rootPrivateKey", help="Set the root private key")
@@ -47,8 +64,6 @@ if args.appName == None:
 	raise Exception("Missing appName")
 if args.appFlags == None:
 	args.appFlags = 0
-if args.bootAddr == None:
-	args.bootAddr = 0xC0D00000
 if args.rootPrivateKey == None:
 	privateKey = PrivateKey()
 	publicKey = str(privateKey.pubkey.serialize(compressed=False)).encode('hex')
@@ -56,6 +71,9 @@ if args.rootPrivateKey == None:
 	args.rootPrivateKey = privateKey.serialize().encode('ascii')
 
 parser = IntelHexParser(args.fileName)
+if args.bootAddr == None:
+    args.bootAddr = parser.getBootAddr()
+
 dongle = getDongle(args.apdu)
 
 if args.deployLegacy:
@@ -71,7 +89,19 @@ appLength = 0
 for area in parser.getAreas():
 	appLength += len(area.getData())	
 
-loader.createApp(args.appFlags, appLength, args.appName)
+icon = None
+if args.icon != None:
+	icon = bytearray.fromhex(args.icon)
+
+signature = None
+if args.signature != None:
+	signature = bytearray.fromhex(args.signature)	
+
+path = None
+if args.path != None:
+	path = parse_bip32_path(args.path)
+
+loader.createApp(args.appFlags, appLength, args.appName, icon, path)
 hash = loader.load(0x0, 0xE0, parser.getAreas(), args.bootAddr)
 print "Application hash : " + hash
-loader.run(parser.getAreas(), args.bootAddr)
+loader.run(parser.getAreas(), args.bootAddr, signature)
