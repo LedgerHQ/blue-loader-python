abe03c2bb8471b9dcccd08fed26325e3c7d96a1a
BTChip <hello@ledger.fr>
2016-08-12 09:56:59 +0200

    Support locking on curve and multiple paths (API level 5)

--- ledgerblue/loadApp.py
+++ ledgerblue/loadApp.py
@@ -28,11 +28,13 @@ import struct
 def auto_int(x):
     return int(x, 0)
 
-def parse_bip32_path(path):
+def parse_bip32_path(path, apilevel):
         if len(path) == 0:
                 return ""
         result = ""
         elements = path.split('/')
+        if apilevel >= 5:
+        	result = result + chr(len(elements))
         for pathElement in elements:
                 element = pathElement.split('\'')
                 if len(element) == 1:
@@ -45,7 +47,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--targetId", help="Set the chip target ID", type=auto_int)
 parser.add_argument("--fileName", help="Set the file name to load")
 parser.add_argument("--icon", help="Set the icon content to use (hex encoded)")
-parser.add_argument("--path", help="BIP 32 path to which the derivation is locked (format decimal a'/b'/c)")
+parser.add_argument("--curve", help="Curve on which the derivation is locked (secp256k1|prime256r1|ed25519), can be repeated", action='append')
+parser.add_argument("--path", help="BIP 32 path to which the derivation is locked (format decimal a'/b'/c), can be repeated", action='append')
 parser.add_argument("--appName", help="Set the application name")
 parser.add_argument("--signature", help="Optional application's signature (hex encoded)")
 parser.add_argument("--appFlags", help="Set the application flags", type=auto_int)
@@ -53,9 +56,12 @@ parser.add_argument("--bootAddr", help="Set the boot address", type=auto_int)
 parser.add_argument("--rootPrivateKey", help="Set the root private key")
 parser.add_argument("--apdu", help="Display APDU log", action='store_true')
 parser.add_argument("--deployLegacy", help="Use legacy deployment API", action='store_true')
+parser.add_argument("--apilevel", help="Use given API level when interacting with the device", type=auto_int)
 
 args = parser.parse_args()
 
+if args.apilevel == None:
+	args.apilevel = 5
 if args.targetId == None:
 	args.targetId = 0x31000002
 if args.fileName == None:
@@ -73,6 +79,35 @@ if args.rootPrivateKey == None:
 parser = IntelHexParser(args.fileName)
 if args.bootAddr == None:
     args.bootAddr = parser.getBootAddr()
+
+path = ""
+curveMask = 0xff
+if args.curve != None:	
+	curveMask = 0x00
+	for curve in args.curve:
+		if curve == 'secp256k1':
+			curveMask |= 0x01
+		elif curve == 'prime256r1':
+			curveMask |= 0x02
+		elif curve == 'ed25519':
+			curveMask |= 0x04
+		else:
+			raise Exception("Unknown curve " + curve)	
+
+if args.apilevel >= 5:
+	path += chr(curveMask)
+	if args.path != None:
+		for item in args.path:
+			if len(item) <> 0:
+				path += parse_bip32_path(item, args.apilevel)	
+else:
+	if args.curve != None:
+		print "Curve not supported using this API level, ignoring"
+	if args.path != None:
+		if len(args.path) > 1:
+			print "Multiple path levels not supported using this API level, ignoring"
+		else:
+			path = parse_bip32_path(args.path[0], args.apilevel)
 
 dongle = getDongle(args.apdu)
 
@@ -96,10 +131,6 @@ if args.icon != None:
 signature = None
 if args.signature != None:
 	signature = bytearray.fromhex(args.signature)	
-
-path = None
-if args.path != None:
-	path = parse_bip32_path(args.path)
 
 loader.createApp(args.appFlags, appLength, args.appName, icon, path)
 hash = loader.load(0x0, 0xE0, parser.getAreas(), args.bootAddr)
