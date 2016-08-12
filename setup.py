4026eb55cf57612cff6c62669d501c2d48af74b7
BTChip <hello@ledger.fr>
2016-08-12 10:00:28 +0200

    Tag 0.1.5, tweaks for PyPI compatibility

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@
 
 from setuptools import setup, find_packages
 from os.path import dirname, join
+import os
+
+os.environ['SECP_BUNDLED_EXPERIMENTAL'] = "1"
 
 here = dirname(__file__)
 setup(
     name='ledgerblue',
-    version='0.1.4',
+    version='0.1.5',
     author='Ledger',
     author_email='hello@ledger.fr',
     description='Python library to communicate with Ledger Blue/Nano S',
     long_description=open(join(here, 'README.md')).read(),
+    url='https://github.com/LedgerHQ/blue-loader-python',
     packages=find_packages(),
     install_requires=['hidapi>=0.7.99', 'secp256k1>=0.12.1', 'pycrypto>=2.6.1'],
     extras_require = {
@@ -22,7 +26,7 @@ setup(
     classifiers=[
 	'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
-        'Operating System :: POSIX :: Windows',
+        'Operating System :: Microsoft :: Windows',
 	'Operating System :: MacOS :: MacOS X'
     ]
 )
