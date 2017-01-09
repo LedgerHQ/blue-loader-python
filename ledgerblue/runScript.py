e728a7b57bed45b6d464e66383fc3d591ae0a6eb
Michael Goodwin <xenithorb@fedoraproject.org>
2017-01-09 14:25:57 -0500

    Fix indent errors and print function for python3 builds

--- ledgerblue/runScript.py
+++ ledgerblue/runScript.py
@@ -25,76 +25,81 @@ import argparse
 import sys
 import fileinput
 
+
 def auto_int(x):
-	return int(x, 0)
+    return int(x, 0)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--fileName", help="Set the file name to load")
 parser.add_argument("--apdu", help="Display APDU log", action='store_true')
-parser.add_argument("--scp", help="open secure channel to exchange apdu", action='store_true')
+parser.add_argument(
+    "--scp", help="open secure channel to exchange apdu", action='store_true')
 parser.add_argument("--targetId", help="Set the chip target ID", type=auto_int)
 parser.add_argument("--rootPrivateKey", help="Set the root private key")
 
 args = parser.parse_args()
 
 if args.targetId is None:
-	args.targetId = 0x31100002
+    args.targetId = 0x31100002
 if not args.fileName:
-	#raise Exception("Missing fileName")
-	file = sys.stdin
+    #raise Exception("Missing fileName")
+    file = sys.stdin
 else:
-	file = open(args.fileName, "r")
+    file = open(args.fileName, "r")
 
 
 class SCP:
-	def __init__(self, dongle, targetId, rootPrivateKey):
-		self.key = getDeployedSecretV2(dongle, rootPrivateKey, targetId)
-		self.iv = b'\x00' * 16
 
-	def encryptAES(self, data):
-		paddedData = data + b'\x80'
-		while (len(paddedData) % 16) != 0:
-			paddedData += b'\x00'
-		cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-		encryptedData = cipher.encrypt(paddedData)
-		self.iv = encryptedData[len(encryptedData) - 16:]
-		return encryptedData
+    def __init__(self, dongle, targetId, rootPrivateKey):
+        self.key = getDeployedSecretV2(dongle, rootPrivateKey, targetId)
+        self.iv = b'\x00' * 16
 
-        def decryptAES(self, data):
-                if len(data) == 0:
-                        return data
-                cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-                decryptedData = cipher.decrypt(data)
-                l = len(decryptedData) - 1
-                while (decryptedData[l] != chr(0x80)):
-                        l-=1
-                decryptedData = decryptedData[0:l]
-                self.iv = data[len(data) - 16:]
-                return decryptedData
+    def encryptAES(self, data):
+        paddedData = data + b'\x80'
+        while (len(paddedData) % 16) != 0:
+            paddedData += b'\x00'
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        encryptedData = cipher.encrypt(paddedData)
+        self.iv = encryptedData[len(encryptedData) - 16:]
+        return encryptedData
+
+    def decryptAES(self, data):
+        if len(data) == 0:
+            return data
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        decryptedData = cipher.decrypt(data)
+        l = len(decryptedData) - 1
+        while (decryptedData[l] != chr(0x80)):
+            l -= 1
+        decryptedData = decryptedData[0:l]
+        self.iv = data[len(data) - 16:]
+        return decryptedData
 
 dongle = getDongle(args.apdu)
 if args.scp:
-	if args.rootPrivateKey is None:
-		privateKey = PrivateKey()
-		publicKey = binascii.hexlify(privateKey.pubkey.serialize(compressed=False))
-		print("Generated random root public key : %s" % publicKey)
-		args.rootPrivateKey = privateKey.serialize()
-	scp = SCP(dongle, args.targetId, bytearray.fromhex(args.rootPrivateKey))
+    if args.rootPrivateKey is None:
+        privateKey = PrivateKey()
+        publicKey = binascii.hexlify(
+            privateKey.pubkey.serialize(compressed=False))
+        print("Generated random root public key : %s" % publicKey)
+        args.rootPrivateKey = privateKey.serialize()
+    scp = SCP(dongle, args.targetId, bytearray.fromhex(args.rootPrivateKey))
 
 for data in file:
-	data = data.rstrip('\r\n').decode('hex')
-	if len(data) < 5:
-		continue
-	if args.scp:
-		data = bytearray(data)
-		if data[4] > 0 and len(data)>5:
-			apduData = data[5 : 5 + data[4]]
-			apduData = scp.encryptAES(str(apduData))
-			result = dongle.exchange(data[0:4] + bytearray([len(apduData)]) + bytearray(apduData))
-		else:
-			result = dongle.exchange(data[0:5])
-		result = scp.decryptAES(str(result))
-		if args.apdu:
-			print("<= Clear " + result.encode('hex'))
-	else:
-		dongle.exchange(bytearray(data))
+    data = data.rstrip('\r\n').decode('hex')
+    if len(data) < 5:
+        continue
+    if args.scp:
+        data = bytearray(data)
+        if data[4] > 0 and len(data) > 5:
+            apduData = data[5: 5 + data[4]]
+            apduData = scp.encryptAES(str(apduData))
+            result = dongle.exchange(
+                data[0:4] + bytearray([len(apduData)]) + bytearray(apduData))
+        else:
+            result = dongle.exchange(data[0:5])
+        result = scp.decryptAES(str(result))
+        if args.apdu:
+            print("<= Clear " + result.encode('hex'))
+    else:
+        dongle.exchange(bytearray(data))
