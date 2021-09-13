9c6080ff12c0c3a81815bd6cd02410fd843873ae
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2021-09-13 17:47:23 +0200

    Bump version 0.1.38

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 here = dirname(__file__)
 setup(
     name='ledgerblue',
-    version='0.1.37',
+    version='0.1.38',
     author='Ledger',
     author_email='hello@ledger.fr',
     description='Python library to communicate with Ledger Blue/Nano S',
