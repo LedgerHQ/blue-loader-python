c8285030835432510378fee8f3fbeac5e4b7a48a
BTChip github <contact@btchip.com>
2022-04-07 09:30:03 +0200

    Add public endorsement endpoint

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os.path import dirname, join
 here = dirname(__file__)
 setup(
     name='ledgerblue',
-    version='0.1.41',
+    version='0.1.42',
     author='Ledger',
     author_email='hello@ledger.fr',
     description='Python library to communicate with Ledger Blue/Nano S',
