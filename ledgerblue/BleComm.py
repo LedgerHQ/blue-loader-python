10abe5fb001034149d536af54c4421a07fa35176
Philippe Bonnaz <philippe@ledger.fr>
2023-04-12 13:07:55 +0200

    Add BleDevice class, with open/close methods, add CLI demo

--- ledgerblue/BleComm.py
+++ ledgerblue/BleComm.py
@@ -1,15 +1,18 @@
 import asyncio
 import argparse
 import os
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
 
 LEDGER_SERVICE_UUID = "13d63400-2c97-6004-0000-4c6564676572"
+HANDLE_CHAR_ENABLE_NOTIF = 13
+HANDLE_CHAR_WRITE = 16
 
 def get_argparser():
     parser = argparse.ArgumentParser(description="Manage ledger ble devices.")
     parser.add_argument("--show", help="Show currently selected ledger device.", action='store_true')
+    parser.add_argument("--demo", help="Connect/Disconnect demo.", action='store_true')
     return parser
 
 class NoLedgerDeviceDetected(Exception):
@@ -39,11 +42,58 @@ class BleScanner(object):
             counter += 1
         await scanner.stop()
 
+queue: asyncio.Queue = asyncio.Queue()
+
+def callback(sender, data):
+    response = bytes(data)
+    queue.put_nowait(response)
+
+async def _get_client(address: str) -> BleakClient:
+    # Connect to client
+    client = BleakClient(address)
+    await client.connect()
+
+    # Register notifications callback
+    await client.start_notify(HANDLE_CHAR_ENABLE_NOTIF, callback)
+
+    # Enable notifications
+    await client.write_gatt_char(HANDLE_CHAR_WRITE, bytes.fromhex("0001"), True)
+    assert await queue.get() == b"\x00\x00\x00\x00\x00"
+
+    # confirm that the MTU is 0x99
+    await client.write_gatt_char(HANDLE_CHAR_WRITE, bytes.fromhex("0800000000"), True)
+    assert await queue.get() == b"\x08\x00\x00\x00\x01\x99"
+
+    return client
+
+class BleDevice(object):
+    def __init__(self, address):
+        self.address = address
+        self.loop = None
+        self.client = None
+        self.opened = False
+
+    def open(self):
+        self.loop = asyncio.get_event_loop()
+        self.client = self.loop.run_until_complete(_get_client(self.address))
+        self.opened = True
+
+    def close(self):
+        if self.opened:
+            self.loop = asyncio.get_event_loop()
+            self.loop.run_until_complete(self.client.disconnect())
+            self.opened = False
+            self.loop.close()
+
 if __name__ == "__main__":
     args = get_argparser().parse_args()
     try:
         if args.show:
             print(f"Environment variable LEDGER_BLE_MAC currently set to '{os.environ['LEDGER_BLE_MAC']}'")
+        elif args.demo:
+            ble_device = BleDevice(os.environ['LEDGER_BLE_MAC'])
+            ble_device.open()
+            ble_device.close()
         else:
             scanner = BleScanner()
             asyncio.run(scanner.scan())
