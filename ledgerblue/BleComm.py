af08df5da38baba5e2e2612ee42ba0e69869b220
Philippe Bonnaz <philippe@ledger.fr>
2023-04-12 10:24:22 +0200

    Add arg parser to BleComm.py

--- ledgerblue/BleComm.py
+++ ledgerblue/BleComm.py
@@ -1,9 +1,19 @@
 import asyncio
+import argparse
+import os
 from bleak import BleakScanner
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
 
 LEDGER_SERVICE_UUID = "13d63400-2c97-6004-0000-4c6564676572"
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description="Manage ledger ble devices.")
+    parser.add_argument("--show", help="Show currently selected ledger device.", action='store_true')
+    return parser
+
+class NoLedgerDeviceDetected(Exception):
+    pass
 
 class BleScanner(object):
     def __init__(self):
@@ -30,12 +40,26 @@ class BleScanner(object):
         await scanner.stop()
 
 if __name__ == "__main__":
-    scanner = BleScanner()
-    asyncio.run(scanner.scan())
-    devices_str = ""
-    device_idx = 0
-    for device in scanner.devices:
-        devices_str += f"  -{device_idx+1}- mac=\"{device[0]}\" name=\"{device[1]}\"\n"
-        device_idx += 1
-    index = int(input(f"Select device by index\n{devices_str}"))
-    print(f"Run 'export LEDGER_BLE_MAC=\"{scanner.devices[index-1][0]}\"'\nThen run your script again")
+    args = get_argparser().parse_args()
+    try:
+        if args.show:
+            print(f"Environment variable LEDGER_BLE_MAC currently set to '{os.environ['LEDGER_BLE_MAC']}'")
+        else:
+            scanner = BleScanner()
+            asyncio.run(scanner.scan())
+            devices_str = ""
+            device_idx = 0
+            if len(scanner.devices):
+                for device in scanner.devices:
+                    devices_str += f"  -{device_idx+1}- mac=\"{device[0]}\" name=\"{device[1]}\"\n"
+                    device_idx += 1
+                index = int(input(f"Select device by index\n{devices_str}"))
+                os.environ['LEDGER_BLE_MAC']=scanner.devices[index-1][0]
+                print(f"Environment variable LEDGER_BLE_MAC succesfully set to '{scanner.devices[index-1][0]}'")
+            else:
+                raise NoLedgerDeviceDetected
+    except NoLedgerDeviceDetected as ex:
+        print(ex)
+    except Exception as ex:
+        raise ex
+    
